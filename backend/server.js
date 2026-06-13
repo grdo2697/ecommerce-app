24ea2bfb80cf01d9273a3ffa01e60d174521aa0f
grdo2 <grdo2697@gmail.com>
2026-06-13 03:10:10 +0300

    fix port 8080

--- backend/server.js
+++ backend/server.js
@@ -133,7 +133,7 @@ app.use((err, req, res, next) => {
 // ============================
 // Start Server
 // ============================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`
   ╔════════════════════════════════════╗
